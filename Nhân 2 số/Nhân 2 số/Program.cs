bf43266d772db445a87cc52aa6145e8557522d9f
NguyenThiLinhKhanh <Linhkhanh.210503@gmail.com>
2024-07-20 10:47:07 +0700

    add LinhKhanh files

---
+++ Nhân 2 số/Nhân 2 số/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;// khai báo để sử dụng biết bigintger
+namespace Nhân_2_số
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BigInteger num1, num2, product;
+
+            Console.Write("Enter the first large integer: ");
+            num1 = BigInteger.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second large integer: ");
+            num2 = BigInteger.Parse(Console.ReadLine());
+
+            product = num1 * num2;
+
+            Console.WriteLine("The product of " + num1 + " and " + num2 + " is: " + product);
+        }
+    }
+}
